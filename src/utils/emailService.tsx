af956daeef9f0fac8d98803c7390d5af20a84bba
Smritiii29 <10h09smriti29@gmail.com>
2025-12-28 00:34:20 +0530

    partially updated tsx files part1

---
+++ src/utils/emailService.tsx
@@ -0,0 +1,45 @@
+// import emailjs from "@emailjs/browser";
+
+// export const sendPatientCredentials = async ({
+//   patientName,
+//   patientEmail,
+//   tempPassword,
+// }) => {
+//   return emailjs.send(
+//     import.meta.env.VITE_EMAILJS_SERVICE_ID,
+//     import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+//     {
+//       patient_name: patientName,
+//       patient_email: patientEmail,
+//       temp_password: tempPassword,
+//       login_link: "http://localhost:5173/login",
+//     },
+//     import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+//   );
+// };
+import emailjs from "@emailjs/browser";
+
+// Define the shape of the parameters
+interface PatientCredentialsParams {
+  patientName: string;
+  patientEmail: string;
+  tempPassword: string;
+}
+
+export const sendPatientCredentials = async ({
+  patientName,
+  patientEmail,
+  tempPassword,
+}: PatientCredentialsParams) => {
+  return emailjs.send(
+    import.meta.env.VITE_EMAILJS_SERVICE_ID as string,
+    import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string,
+    {
+      patient_name: patientName,
+      patient_email: patientEmail,
+      temp_password: tempPassword,
+      login_link: "http://localhost:5173/login",
+    },
+    import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string
+  );
+};
